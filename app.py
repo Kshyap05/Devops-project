60a3b4542ad7a830bc1bd87bd6f2eb3efd4e046e
Kshyap05 <saurashtrakashyap1@proton.me>
2026-05-22 07:58:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps World!,my name is kashyap the upcoming enginner"
+    return "Hello, DevOps World!,my name is kashyap the upcoming enginner with all the efforts i will do all this"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
